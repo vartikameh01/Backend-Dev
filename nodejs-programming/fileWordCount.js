a2ca7584bcdbe4c610d1493cb9ac24294841739b
Vartika <mehrotravartika76@gmail.com>
2026-01-23 00:30:09 +0530

    Move nodejs programming files from rest-security

---
+++ nodejs-programming/fileWordCount.js
@@ -0,0 +1,15 @@
+const fs= require("fs");
+fs.readFile("input.txt","utf8",(err,data)=>{ //asynchronous function
+    if(err){
+        console.log("Error reading file");
+        return;
+    }
+    const word=data.trim().split(" ");
+    const wordcount=word.length;    
+    fs.writeFile("output text", `word count:${wordcount}`,(err)=>{
+        if (err) {
+      console.log("Error writing file");
+      return;
+    }
+  });
+});
